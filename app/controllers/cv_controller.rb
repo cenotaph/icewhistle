61a93a5ae6b515e2385884b08b4f692dbdbf00eb
John W. Fail <fail@cenotaph.org>
2015-02-01 14:55:53 +0100

    CV fix

--- app/controllers/cv_controller.rb
+++ app/controllers/cv_controller.rb
@@ -4,11 +4,11 @@ class CvController < ApplicationController
  # caches_action :index, :expires_in => '4 days'
  
   def index
-    @schools = School.find(:all, :order => 'finished DESC')
-    @jobs = Job.find(:all, :order => 'realdate DESC, id DESC')
-    @publications = Publication.find(:all, :order => 'sortorder, realdate DESC, id DESC')
-    @projects = Project.find(:all, :order => 'sortorder, finished DESC, id DESC')
-    @skill = Skill.find(:all)
+    @schools = School.all.order('finished DESC')
+    @jobs = Job.all.order('realdate DESC, id DESC')
+    @publications = Publication.all.order('sortorder, realdate DESC, id DESC')
+    @projects = Project.all.order('sortorder, finished DESC, id DESC')
+    @skill = Skill.all
   end
     
 end
