80ee285e9edd152bf838a56a98c01f5d168b17a3
John W. Fail <john@pixelache.ac>
2015-11-18 13:55:31 +0200

    Icon for retrospections

--- app/controllers/retrospections_controller.rb
+++ app/controllers/retrospections_controller.rb
@@ -4,7 +4,7 @@ class RetrospectionsController < ApplicationController
   def create
     @retrospection = Retrospection.new(retrospection_params)
     @retrospection.ip_address = request.remote_ip
-
+    
     if verify_recaptcha(:model => @retrospection, :message => "Oh! It's error with reCAPTCHA!") && @retrospection.save
       flash[:notice] = 'Your survey has been submitted. Thank you so much for your feedback!'
       redirect_to '/'
@@ -17,14 +17,26 @@ class RetrospectionsController < ApplicationController
   
   def index
     user_ip = request.remote_ip
-    set_meta_tags title: 'Serious Retrospection'
+    @existing = Retrospection.where(ip_address: user_ip)
+    unless @existing.empty?
+      if @existing.map{|x| x.created_at.to_date }.include?(Time.now.to_date)
+        @already_in_db = 1
+      end
+      
+    end
+    set_meta_tags title: 'Serious Retrospection', canonical: 'http://johnw.fail/retrospections',
+                  og: {image: 'http://icewhistle.com/assets/si_box.jpg', 
+                        title: 'Serious Retrospection', type: 'website',
+                         url:'http://johnw.fail/retrospections'
+                      }, 
+                  twitter: {card: 'summary', site: '@hyksos'}
   end
   
   protected
   
   def retrospection_params
     params.require(:retrospection).permit(:name, :age, :episodes_attended, :episodes_watched, :best_cast, :best_guest,
-    :nationality, :humour, :karaoke, :music, :intropsection, :segment_1_culture, :segment_2_dance, :segment_3_distribution,
+    :nationality, :humour, :karaoke, :music, :introspection, :segment_1_culture, :segment_2_dance, :segment_3_distribution,
     :segment_4_mailbag, :segment_5_mjtrivia, :segment_6_showtell, :segment_7_siivous, :segment_8_trivia, 
     :segment_9_visitortasks, :segment_10_justin, :segment_11_sigame, :additional_comments, :email)
   end
