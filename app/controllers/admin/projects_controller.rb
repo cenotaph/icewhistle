6840c803551ff72b8f7de5d5a5820106161a51e1
John W. Fail <john@pixelache.ac>
2016-02-21 14:21:49 +0200

    cv controller ixes

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,7 +1,43 @@
 class Admin::ProjectsController <  Admin::BaseController
+ respond_to :html
+ 
+ def new
+   @project = Project.new
+ end
+ 
+ def edit
+   @project = Project.find(params[:id])
+ end
+ 
+ def create
+   @project = Project.new(permitted_params)
+   if @project.save
+     respond_with @project, location: -> { admin_projects_path }
+   end
+   
+ end
+ 
+ def destroy
+   @project = Project.find(params[:id])
+   @project.destroy!
+   redirect_to admin_projects_path
+ end
+ 
+ def update
+   @project = Project.find(params[:id])
+   if @project.update_attributes(permitted_params)
+     respond_with @project, location: -> { admin_projects_path }
+   end
+ end
+ 
+
+ def index
+   @projects = Project.all
+ end
+ 
   protected
   
   def permitted_params
-    params.permit(:project => [:name, :projecttype, :url1, :url2, :started, :finished, :description, :major, :sortorder ])
+    params.require(:project).permit(:name, :projecttype, :url1, :url2, :started, :finished, :description, :major, :sortorder)
   end
 end
--- app/controllers/admin/publications_controller.rb
+++ app/controllers/admin/publications_controller.rb
@@ -1,9 +1,45 @@
 class Admin::PublicationsController < Admin::BaseController
+  
+  
+   respond_to :html
+  def new
+    @publication = Publication.new
+  end
+  
+  def edit
+    @publication = Publication.find(params[:id])
+  end
+  
+  def create
+    @publication = Publication.new(permitted_params)
+    if @publication.save
+      respond_with @publication, location: -> { admin_publications_path }
+    end
+    
+  end
+  
+  def destroy
+    @publication = Publication.find(params[:id])
+    @publication.destroy!
+    redirect_to admin_publications_path
+  end
+  
+  def update
+    @publication = Publication.find(params[:id])
+    if @publication.update_attributes(permitted_params)
+      respond_with @publication, location: -> { admin_publications_path }
+    end
+  end
+  
 
+  def index
+    @publications = Publication.all
+  end
+  
   protected
   
   def permitted_params
-    params.permit(:publication => [:title, :format, :publisher, :date, :description, :major, :realdate, :icon, :sortorder ])
+    params.require(:publication).permit(:title, :format, :publisher, :date, :description, :major, :realdate, :icon, :sortorder)
   end
     
 end
