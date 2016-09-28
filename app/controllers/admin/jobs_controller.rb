a1361bde7bc00c98bf1be664cc3a65b8558e1491
John W. Fail <john@pixelache.ac>
2016-09-28 16:04:19 +0300

    strong params

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,12 +1,40 @@
-class Admin::JobsController < Admin::BaseController
-  def create
-    create! { admin_jobs_path }
-  end
-  
-  def update
-    update! { admin_jobs_path }
-  end
-  
+class Admin::JobsController <  Admin::BaseController
+ respond_to :html
+ 
+ def new
+   @job = Job.new
+ end
+ 
+ def edit
+   @job = Job.find(params[:id])
+ end
+ 
+ def create
+   @job = Job.new(permitted_params)
+   if @job.save
+     respond_with @job, location: -> { admin_jobs_path }
+   end
+   
+ end
+ 
+ def destroy
+   @job = Job.find(params[:id])
+   @job.destroy!
+   redirect_to admin_jobs_path
+ end
+ 
+ def update
+   @job = Job.find(params[:id])
+   if @job.update_attributes(permitted_params)
+     respond_with @job, location: -> { admin_jobs_path }
+   end
+ end
+ 
+
+ def index
+   @jobs = Job.all
+ end
+
   
   protected
   
--- app/controllers/admin/skills_controller.rb
+++ app/controllers/admin/skills_controller.rb
@@ -1,7 +1,41 @@
-class Admin::SkillsController < Admin::BaseController
-  
-  protected
-  
+class Admin::SkillsController <  Admin::BaseController
+ respond_to :html
+ 
+ def new
+   @skill = Skill.new
+ end
+ 
+ def edit
+   @skill = Skill.find(params[:id])
+ end
+ 
+ def create
+   @skill = Skill.new(permitted_params)
+   if @skill.save
+     respond_with @skill, location: -> { admin_skills_path }
+   end
+   
+ end
+ 
+ def destroy
+   @skill = Skill.find(params[:id])
+   @skill.destroy!
+   redirect_to admin_skills_path
+ end
+ 
+ def update
+   @skill = Skill.find(params[:id])
+   if @skill.update_attributes(permitted_params)
+     respond_with @skill, location: -> { admin_skills_path }
+   end
+ end
+ 
+
+ def index
+   @skills = Skill.all
+ end
+
+ private 
   def permitted_params
     params.permit(:skill => [:skills, :skilltype])
   end
