ab8b7b8256bec155bf062e0d7a6caf0263865993
John W. Fail <john@pixelache.ac>
2019-01-06 12:19:07 +0200

    upgrade to Ruby 2.6.0 and Sphinx 3

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
  
 
   def index
-    @posts = ThinkingSphinx.search(params[:search],:order => 'created_at DESC').to_a.delete_if{|x| !x.published }
+    @posts = Post.search(params[:search],:order => 'created_at DESC').to_a.delete_if{|x| !x.published }
     @posts = Kaminari.paginate_array(@posts).page(params[:page]).per(10)
     if @posts.size == 0
     	flash[:notice] = 'Sorry, no results found for '+params[:search]
