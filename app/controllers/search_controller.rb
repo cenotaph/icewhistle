ff9a54cca168e541b3e2ca05bf261f5c01201a74
John W. Fail <john@pixelache.ac>
2019-01-09 16:02:34 +0200

    fix nil search issue

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,21 @@
 class SearchController < ApplicationController
- 
+  respond_to :html 
 
   def index
-    @posts = Post.search(params[:search],:order => 'created_at DESC').to_a.delete_if{|x| !x.published }
-    @posts = Kaminari.paginate_array(@posts).page(params[:page]).per(10)
-    if @posts.size == 0
-    	flash[:notice] = 'Sorry, no results found for '+params[:search]
+    if params[:search].blank?
+      flash[:notice] = 'Please enter a search term.'
+      redirect_to posts_path and return
     else
-    	flash[:notice] = 'Search results for <b><i>'+ params[:search] + '</b></i>:'
+      @posts = Post.search(params[:search],:order => 'created_at DESC').to_a.delete_if{|x| !x.published }
+      @posts = Kaminari.paginate_array(@posts).page(params[:page]).per(10)
+      if @posts.size == 0
+      	flash[:notice] = 'Sorry, no results found for '+params[:search]
+      else
+      	flash[:notice] = 'Search results for <b><i>'+ params[:search] + '</b></i>:'
+      end
+      
+      @search = params[:search]
+      render :template => 'posts/index'
     end
-    
-    @search = params[:search]
-    render :template => 'posts/index'
-
   end
-
 end
