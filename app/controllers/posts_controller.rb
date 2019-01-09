352e99174856a09b8540115f1bba042dc3b7a960
John W. Fail <john@pixelache.ac>
2019-01-09 16:46:32 +0200

    discard old posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,18 +8,23 @@ class PostsController < ApplicationController
   end
   
   def show
-
     @post = Post.friendly.find(params[:id])
-    set_meta_tags :title => @post.title, 
-                  canonical: url_for(@post),
-                  og: {image: (@post.image? ?  [ @post.image.url(:midsize).gsub(/^https/, 'http'), { secure_url: @post.image.url(:midsize) } ] : 'http://icewhistle.com/icewhistle.jpg'), 
-                        title: @post.title, type: 'website', url: url_for(@post),
-                        description: ActionView::Base.full_sanitizer.sanitize(@post.body[0..500]) + "..."
-                      }, 
-                  twitter: {card: 'summary', site: '@hyksos'}
-    respond_to do |format|
-      format.html { render layout: get_layout }
-      format.all  { render(text: 'Not Found', status: :unsupported_media_type) }
+    if @post.discarded? && !user_signed_in?
+      flash[:notice] = 'This post has been deleted.'
+      redirect_to '/posts' and return
+    else
+
+      set_meta_tags :title => @post.title, 
+                    canonical: url_for(@post),
+                    og: {image: (@post.image? ?  [ @post.image.url(:midsize).gsub(/^https/, 'http'), { secure_url: @post.image.url(:midsize) } ] : 'http://icewhistle.com/icewhistle.jpg'), 
+                          title: @post.title, type: 'website', url: url_for(@post),
+                          description: ActionView::Base.full_sanitizer.sanitize(@post.body[0..500]) + "..."
+                        }, 
+                    twitter: {card: 'summary', site: '@hyksos'}
+      respond_to do |format|
+        format.html { render layout: get_layout }
+        format.all  { render(text: 'Not Found', status: :unsupported_media_type) }
+      end
     end
   end
   
