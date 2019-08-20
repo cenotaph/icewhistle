8568007d664d3cbf93eeb13fd5066f6fcb601377
John W. Fail <john@pixelache.ac>
2019-08-20 16:51:09 +0300

    try to stop recaptcha spam

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -21,18 +21,22 @@ class ContactController < ApplicationController
   end
   
   def create
-    @message = Message.new(params[:message])
-    
-    if @message.valid?
-      if verify_recaptcha(:model => @message, :message => "You have entered an incorrect CAPTCHA.")
-        NotificationsMailer.new_message(@message).deliver
-        redirect_to(root_path, :notice => "Your message was successfully sent.")
+    begin
+      @message = Message.new(params[:message])
+   
+      if @message.valid?
+        if verify_recaptcha(:model => @message, :message => "You have entered an incorrect CAPTCHA.")
+          NotificationsMailer.new_message(@message).deliver
+          redirect_to(root_path, :notice => "Your message was successfully sent.")
+        else
+          render :template => 'contact/new'
+        end
       else
-        render :template => 'contact/new'
+        flash.now.alert = "Please fill all fields."
+        render :new
       end
-    else
-      flash.now.alert = "Please fill all fields."
-      render :new
+    rescue  ArgumentError => e
+      render plain: 'Go away', status: 422
     end
   end
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,12 +12,12 @@ class PostsController < ApplicationController
       flash[:notice] = 'This post has been deleted.'
       redirect_to '/posts' and return
     else
-      set_meta_tags :title => @post.title, 
+      set_meta_tags :title => @post.title,
                     canonical: url_for(@post),
                     og: {image: (@post.image? ?  [ @post.image.url(:midsize).gsub(/^https/, 'http'), { secure_url: @post.image.url(:midsize) } ] : 'http://icewhistle.com/icewhistle.jpg'), 
                           title: @post.title, type: 'website', url: url_for(@post),
                           description: ActionView::Base.full_sanitizer.sanitize(@post.body[0..500]) + "..."
-                        }, 
+                        },
                     twitter: {card: 'summary', site: '@hyksos'}
       respond_to do |format|
         format.html { render layout: get_layout }
